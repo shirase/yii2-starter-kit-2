c0bbd0df0d09775000ea6c585a3fe0c6982277db
Andrey Akimov <shirase55@gmail.com>
2019-12-19 16:55:39 +0300

    backend url fix

--- backend/config/_urlManager.php
+++ backend/config/_urlManager.php
@@ -4,8 +4,6 @@ return [
     'enablePrettyUrl' => true,
     'showScriptName' => false,
     'rules' => [
-        ['pattern' => '/', 'route' => 'site/index'],
-
         ['pattern'=>'<controller>', 'route'=>'<controller>/index'],
         ['pattern'=>'<controller>/<action>', 'route'=>'<controller>/<action>'],
         ['pattern'=>'<module>/<controller>', 'route'=>'<module>/<controller>/index'],
